5eb1830974ffc2ebeeab95f42c6ce6aadb3ae744
REX MAN (EUCD-EUC-ISD-OOCLL/ZHA) <rex.man@oocl.com>
2020-01-07 09:41:43 +0800

    添加变量部分

--- Base.dart
+++ Base.dart
@@ -1,5 +1,72 @@
 main(){
     
+
+    // 变量
+var name='Bob';  
+print('The name is $name');
+/* 
+  var 定义的变量，变量类型是通过变量值推断出来的
+  如果变量赋过初值，则表明变量类型已经确定，
+  这时此变量不可在将其他类型的值付给他。
+
+  name=12; //此句放开会报错。 Error: A value of type 'int' can't be assigned to a variable of type 'String'.
+  print('The name is $name');
+*/
+
+//使用特定类型 来定义变量
+String nameString='bruce';
+print('The name is $nameString');
+
+int age=18;
+print('The age is $age');
+
+//如果变量不限于单个类型，可以使用dynamic或者object来定义变量
+dynamic value = 18 ;
+print("value=$value");
+value = 'bruce';
+print("value=$value");
+
+Object val =18;
+print('val = $val');
+val ='Tiger';
+print('val = $val');
+
+/*
+   由于Dart 能够放在变量中的所有内容都是对象，所以如果变量没有初始化，
+   那他的默认值就是null。
+*/
+var varNotInit;
+    print('varNotInit = $varNotInit');
+String stringNotInit;
+    print('stringNotInit = $stringNotInit');
+dynamic dynamicNotInit;
+    print('dynamicNotInit = $dynamicNotInit');
+
+/*
+    如果变量的值不发生变化，那么可以使用final 或者const修饰，
+    final 变量只能设置一次，const变量是编译时常量
+ */
+//类型声明可以省略, 普通的变量是不允许省略的，
+final String afinal='abc';
+print('afinal = $afinal');
+final bfinal=34;
+print('bfinal = $bfinal');
+
+const String aConst='aConst';
+print('aConst = $aConst');
+const bConst='bConst';
+print('bConst = $bConst');
+
+//初始化后不能在赋值
+//afinal='cdf'; //Error: Setter not found: 'afinal'. 因为final修饰的变量不能调用其setter方法
+//aConst='ccs'; //Error: Setter not found: 'aConst'. const修饰的变量不能调用setter方法
+
+//final 和 const 不能和 var 同时使用
+//final var aFinalVar = 'abc';   //Error: Members can't be declared to be both 'final' and 'var'.
+//const var aConstVar = 'test';  //Error: Members can't be declared to be both 'var' and 'const'.
+
+final dynamic aDynamic = '';
+
     //const 可以使用其他const常量的值来初始化其值
     const first=100;
     const second=20;
