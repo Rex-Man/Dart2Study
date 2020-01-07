b9cf5e8bb97f03f4a60f3131e0a2bda1c31bd42b
REX MAN (EUCD-EUC-ISD-OOCLL/ZHA) <rex.man@oocl.com>
2020-01-07 14:41:15 +0800

    添加理解

--- Base.dart
+++ Base.dart
@@ -1,7 +1,6 @@
 main(){
-    
 
-    // 变量
+// 变量
 var name='Bob';  
 print('The name is $name');
 /* 
@@ -46,7 +45,7 @@ dynamic dynamicNotInit;
     如果变量的值不发生变化，那么可以使用final 或者const修饰，
     final 变量只能设置一次，const变量是编译时常量
  */
-//类型声明可以省略, 普通的变量是不允许省略的，
+//类型声明可以省略 如果为 final 和 const 类型，
 final String afinal='abc';
 print('afinal = $afinal');
 final bfinal=34;
@@ -57,7 +56,7 @@ print('aConst = $aConst');
 const bConst='bConst';
 print('bConst = $bConst');
 
-//初始化后不能在赋值
+//初始化后不能再次赋值
 //afinal='cdf'; //Error: Setter not found: 'afinal'. 因为final修饰的变量不能调用其setter方法
 //aConst='ccs'; //Error: Setter not found: 'aConst'. const修饰的变量不能调用setter方法
 
@@ -65,70 +64,84 @@ print('bConst = $bConst');
 //final var aFinalVar = 'abc';   //Error: Members can't be declared to be both 'final' and 'var'.
 //const var aConstVar = 'test';  //Error: Members can't be declared to be both 'var' and 'const'.
 
-final dynamic aDynamic = '';
+//const 可以使用其他const常量的值来初始化其值
+const first=100;
+const second=20;
+const result = first+second;
+print("const result=$result");
+/* 
+  如果 用非const 的值来操作会报错
+  var third=40;
+  const result2=first + third;  //Error: Not a constant expression.
+  print("const result=$result2");
 
-    //const 可以使用其他const常量的值来初始化其值
-    const first=100;
-    const second=20;
-    const result = first+second; // 
-    print("const result=$result");
-    /* 
-      var third=40;
-      const result2=first + third;  //Error: Not a constant expression.
-      print("const result=$result2");
+  int third =3;
+  const result3=first + third;  //Error: Not a constant expression.
+  print("const result=$result3");
+*/
 
-      int third =3;
-      const result3=first + third;  //Error: Not a constant expression.
-      print("const result=$result3");
-    */
-    
-    //const初始化时需要一个确定的值，final不需要
-    int dt=33;
-    int  dtConst = TestFunction(dt);
-    print("dtConst result=$dtConst"); //dtConst result=33
+// final 这没有const 那种限制，可以与非final 数据操作。
+final firstFinal=100;
+final secondFinal=20;
+final resultFinal = firstFinal + secondFinal;
+print('final resultFinal $resultFinal');
+
+var thirdFinal=30;
+final resultFinal2 = firstFinal+ thirdFinal;
+print('final resultFinal2 $resultFinal2');
+
+//const初始化时需要一个确定的值，final不需要 ， 换句话说，const 在定义的时候一定要给一个明确的值。
+int dt=33;
+int dtConst = TestFunction(dt);
+print("dtConst result=$dtConst"); //dtConst result=33
 
 /**
  * const 在等号的右面，则这个变量是可以重新赋值的。包括修改某一个值
- * const 在等号的左面，则这个变量是不可以重新赋值的。包括修改某一个值
  */
-    var foo = const[];
-    foo=[1,2];
+  var foo = const[];
+  foo=[1,2];
     print('print foo:$foo'); //print foo:[1, 2]
     foo[0]=13;
     print('print foo:$foo'); //print foo:[13, 2]
 
-    // const  fooconstleft=[11,12];
-    // fooconstleft=[1,2]; //Error: Setter not found: 'fooconst'.
-    // print('print fooconst:$fooconst');
-    // fooconstleft[0]=12;//此句出错。 Unsupported operation: Cannot modify an unmodifiable list
-    // print('print fooconstleft:$fooconstleft');
+/* 
+ * const 在等号的左面，则这个变量是不可以重新赋值的。包括修改某一个值
+ 
+    const fooconstleft=[11,12];
+    fooconstleft=[1,2]; //Error: Setter not found: 'fooconst'.
+    print('print fooconst:$fooconst');
+    fooconstleft[0]=12;//此句出错。 Unsupported operation: Cannot modify an unmodifiable list
+    print('print fooconstleft:$fooconstleft');
+*/
 
-/**
+/** 
+ * final 在等号的右面可以改变这个变量的值，也可以修改某个具体的值
+ */
+final finallist=[11,22];
+var fooval = finallist;
+print('fooval:$fooval');     //fooval:[11, 22]
+fooval=[33,44];
+print('foofinal:$fooval');   //foofinal:[33, 44]
+fooval[0]=44;
+print('foofinal:$fooval');   //foofinal:[44, 44]
+/*
  * final 在等号的左面 ，改变这个变量的值，报错，但是修改某一个值，则可以修改。
  * 原因是 final 定义的变量 整体修改违反了final 只赋值一次的原则。
  */
-    final finallist=[11,22];
-    var fooval = finallist;
-    print('fooval:$fooval');     //fooval:[11, 22]
-    fooval=[33,44];
-    print('foofinal:$fooval');   //foofinal:[33, 44]
-    fooval[0]=44;
-    print('foofinal:$fooval');   //foofinal:[44, 44]
 
+  final foofinalleft=[13,14];
+  //foofinalleft=[22,33]; //Error: Setter not found: 'foofinalleft'.
+  print('foofinalleft:$foofinalleft');
+  foofinalleft[0]=99;
+  print('foofinalleft:$foofinalleft');  //foofinalleft:[99, 14]
 
-     final foofinalleft=[13,14];
-     //foofinalleft=[22,33]; //Error: Setter not found: 'foofinalleft'.
-     print('foofinalleft:$foofinalleft');
-     foofinalleft[0]=99;
-     print('foofinalleft:$foofinalleft');  //foofinalleft:[99, 14]
-
-    /* 
-       值相同时final在内存中重复创建，const会引用相同值
-    */
+/* 
+ * 值相同时 final在内存中重复创建，const会引用相同值,个人理解原因是const 定义的变量，是不允许第二次赋值的。
+ */
     final a1 = [11 , 22];
     final a2 = [11 , 22];
     print(identical(a1, a2));//false
-
+    
     const a3 = [11 , 22];
     const a4 = [11 , 22];
     print(identical(a3, a4));//true
